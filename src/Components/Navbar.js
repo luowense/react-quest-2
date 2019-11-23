d6612aabb1f7102d65472c9152c5b0b49cd9ce21
Laurent Segura <54800835+luowense@users.noreply.github.com>
2019-11-23 16:23:05 +0100

    navbar finished

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+            <h1><a href="/">Simpson's Wikipedia Page</a></h1>
+        </nav>
+    );
+}
+
+export default Navbar;
+
